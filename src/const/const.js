de37a8f1f4067113936da3796b34b1914f8b0cf6
mohamedBenma <mbenmaouche09@gmail.com>
2024-12-08 20:57:43 +0100

    add portfolio project

---
+++ src/const/const.js
@@ -0,0 +1,320 @@
+export const studies = [
+ 
+  {
+    title: "Licence en Informatique",
+    corporation: "Université de Lille",
+    date: "09/2019 – 09/2023",
+  },
+  {
+    title: "Msc Expert informatique et Systèmes d'information ",
+    corporation: "EPSI LILLE",
+    date: "09/2023 – à ce jour",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  nest:"nest"
+  
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.nest,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind,
+  },
+  {
+    title: "React Js",
+    icon: IconKeys.react,
+  },
+  {
+    title: "Next Js",
+    icon: IconKeys.nextjs,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Material UI",
+    icon: IconKeys.materialUi,
+  },
+
+  {
+    title: "Redux Js",
+    icon: IconKeys.redux,
+  },
+  {
+    title: "Styled Components",
+    icon: IconKeys.styled,
+  },
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+  {
+    title: "Radix UI",
+    icon: IconKeys.radix,
+  },
+
+  {
+    title: "React Query",
+    icon: IconKeys.reactQuery,
+  },
+  {
+    title: "Vite",
+    icon: IconKeys.vite,
+  },
+  {
+    title: "Zustand",
+    icon: IconKeys.zustand,
+  },
+
+  {
+    title: "Shadcn UI",
+    icon: IconKeys.shadcn,
+  },
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb,
+  },
+];
+
+export const projects = [
+  {
+    img: "../../../img/tasks-manager.webp",
+    title: "Eviden-CRE",
+    link: "https://task-manager-ecru-chi.vercel.app/dashboard",
+    description: `Administrador de tareas con drag and drop autenticación con Clerk , shadcn.  "En Desarrollo "`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.nextjs,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+      {
+        icon: IconKeys.zustand,
+      },
+      {
+        icon: IconKeys.shadcn,
+      },
+    ],
+  },
+  {
+    img: "../../../img/ecommerce-stripe-2.webp",
+    title: "Eviden-OnBoarding",
+    link: "https://ecommerce-stripe-763m.vercel.app/",
+    description: `Proyecto FullStack de un ecommerce con pasarela de pagos stripe integrada, destalles de el product etc.  `,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.nextjs,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+      {
+        icon: IconKeys.mongodb,
+      },
+      {
+        icon: IconKeys.stripe,
+      },
+    ],
+  },
+  {
+    img: "../../../img/nexanime.webp",
+    title: "The Homies-Home",
+    link: "https://nexanime.vercel.app",
+    description: ` Site de mode nouvelle génération : achetez en ligne comme jamais auparavant !`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.nextjs,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+    ],
+  },
+  {
+    img: "../../../img/store-games.webp",
+    title: "WebMagic",
+    link: "https://store-games-site.netlify.app/",
+    description: `Analyse, refactoring et résolution d'erreurs sur un projet open source de grande 
+envergure.`,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+      {
+        icon: IconKeys.reactQuery,
+      },
+    ],
+  },
+  {
+    img: "../../../img/victory-quest.jpg",
+    title: "Victory Quest",
+    link: "https://movietvsite.netlify.app/",
+    description: `Créez et gérez vos propres compétitions sportives avec ce jeu de simulation ! `,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+    ],
+  },
+  {
+    img: "../../../img/cyberpunk.webp",
+    title: "Sell Yours App",
+    link: "https://preeminent-liger-266356.netlify.app/",
+    description: `Plateforme d'échange d'objets : vendez et achetez facilement !.`,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+    ],
+  },
+
+  {
+    img: "../../../img/fit-nation.webp",
+    title: "gym",
+    link: "https://fitnation.netlify.app/",
+    description: `landing page de una gym por ahora es una landing todavia está en
+              desarrollo la idea es que los usuarios puedan comprar productos de
+              el gym`,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+    ],
+  },
+  {
+    img: "../../../img/pong-game.jpg",
+    title: "Multiplayer-Pong",
+    link: "https://www.linkedin.com/posts/osnaider-mart%C3%ADnez_clon-de-la-p%C3%A1gina-de-spotify-echo-con-react-activity-7119712602342965248-upgL?utm_source=share&utm_medium=member_desktop",
+    description: `Jeu "Pong" en version réseau simplifiée. `,
+    technologies: [
+      {
+        icon: IconKeys.react,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+      {
+        icon: IconKeys.redux,
+      },
+      {
+        icon: IconKeys.reactQuery,
+      },
+    ],
+  },
+
+  {
+    img: "../../../img/netflix-clone.jpg",
+    title: "Netflix-Clone",
+    link: "https://realityvirtual.netlify.app/",
+    description: `Clone de Netflix : Une expérience de streaming réinventée. `,
+    technologies: [
+      {
+        icon: IconKeys.astro,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+    ],
+  },
+];
